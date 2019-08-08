03167201ea33bacac3051e28114dfd2e862ed7bf
Vinh Ngu <vinh-ngu@hotmail.com>
2019-08-08 20:19:06 +0200

    Add initial files.

---
+++ plot.py
@@ -0,0 +1,49 @@
+import datetime
+
+import matplotlib.dates as md
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def get_values():
+    with open("notes.txt", 'r') as file:
+        data = file.read()
+
+    data = [[float(y) for y in x.split()] for x in data.split('\n') if x]
+
+    # print([list(t) for t in zip(*data)])
+    packageloss, reponsetime, time = zip(*data)
+
+    return [[packageloss, reponsetime], np.array(time)]
+
+
+if __name__ == '__main__':
+    vals = get_values()
+    time = vals[1]
+
+    dates = [datetime.datetime.fromtimestamp(ts) for ts in vals[1]]
+    datenums = md.date2num(dates)
+
+    plt.title('Beehive Network Monitor')
+
+    plot1 = plt.subplot(211)
+    # plt.subplots_adjust(bottom=0.5)
+    plt.xticks(rotation=90)
+    ax = plt.gca()
+    xfmt = md.DateFormatter('%Y-%m-%d %H:%M:%S')
+    ax.xaxis.set_major_formatter(xfmt)
+    plt.ylabel('Package Loss (%)')
+    plt.plot(datenums, vals[0][0], '.-')
+
+    plot2 = plt.subplot(212)
+    ax = plt.gca()
+    ax.xaxis.set_major_formatter(xfmt)
+    plt.xticks(rotation=90)
+    plot2.plot(datenums, vals[0][1], '.-')
+    plt.ylabel('Response Time (ms)')
+    plt.xlabel('timestamp')
+    plot1.set_xticklabels([])
+    plot1.get_shared_x_axes().join(plot1, plot2)
+
+    plt.autoscale()
+    plt.show()
---
+++ record.py
@@ -0,0 +1,38 @@
+import subprocess
+import time
+from subprocess import PIPE
+
+
+def get_packageloss(input):
+    packetloss = float(
+        [x for x in input.split('\n') if x.find('packet loss') != -1][0].split('%')[0].split(' ')[-1])
+    return packetloss
+
+
+def get_responsetime(input):
+    responsetime = -1
+    for x in input.split('\n'):
+        if x.find('round-trip min/avg/max/stddev') != -1:
+            responsetime = float(x.split('=')[1].split('/')[1])
+    return responsetime
+
+
+def note_to_file(input):
+    with open('notes.txt', 'a+') as file:
+        file.write(input)
+
+
+if __name__ == '__main__':
+    hostname = "8.8.8.8"
+    while (True):
+        process = subprocess.Popen(['ping', '-c', '3', hostname],
+                                   stdout=PIPE, stderr=PIPE)
+        stdout, stderr = process.communicate()
+        input = stdout.decode('utf-8')
+
+        now = time.mktime(time.localtime())
+        packetloss = get_packageloss(input)
+        responsetime = get_responsetime(input)
+
+        note_to_file("{} {} {}\n".format(packetloss, responsetime, now))
+        print("Package Loss: {} % \t Response Time: {} ms \t Time: {}".format(packetloss, responsetime, now))
