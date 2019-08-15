dd5bacedb7bcc6676e52132df7167579e031549c
Vinh Ngu <vinh-ngu@hotmail.com>
2019-08-15 10:22:59 +0200

    Add title to plot.

--- plot.py
+++ plot.py
@@ -23,18 +23,35 @@ def get_values():
 
 if __name__ == '__main__':
     df = pd.DataFrame(data=get_values())
+    first_date = str(df.get('timestamp')[0])
+    date_day = first_date.split()[0]
+    start_hour = first_date.split()[1]
+    end_hour = str(df.get('timestamp')[df.index[-1]]).split()[1]
+    title = "Beehive Network ({})".format(date_day)
     chart_rt = alt.Chart(df, width=1000, height=500).mark_line().encode(
         x='timestamp:T',
         y='responsetime:Q',
         tooltip=['timestamp:T']
     ).properties(
-        title=""
+        title="{} - {} - {} to {}".format(title, 'ResponseTime', start_hour, end_hour)
+    ).configure_title(
+        fontSize=20,
+        font='Courier',
+        anchor='start',
+        color='gray'
     ).interactive()
 
     chart_pl = alt.Chart(df, width=1000, height=500).mark_line().encode(
         x='timestamp:T',
         y='packageloss:Q',
         tooltip=['timestamp:T']
+    ).properties(
+        title="{} - {} - {} to {}".format(title, 'PackageLoss', start_hour, end_hour),
+    ).configure_title(
+        fontSize=20,
+        font='Courier',
+        anchor='start',
+        color='gray'
     ).interactive()
 
     basename = os.path.splitext(sys.argv[1])[0]
